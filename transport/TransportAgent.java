c4a75a8aa49eb7c8e5c5870968381128ec3e3334
Pablo Chacin <pablochacin@gmail.com>
2009-01-20 19:50:05 +0000

    complete implementation of transport modell.

--- transport/TransportAgent.java
+++ transport/TransportAgent.java
@@ -6,10 +6,11 @@ import edu.upc.cnds.collectives.node.Node;
 import edu.upc.cnds.collectives.protocol.Protocol;
 import edu.upc.cnds.collectives.transport.imp.DynamicProxyTransport;
 import edu.upc.cnds.collectives.underlay.UnderlayNode;
+import edu.upc.cnds.collectives.util.ReflectionUtils;
 
 /**
  * Implements a transport by simulating the communication with another node.
- * Uses the TransportModel to delegate the invocation.
+ * Delegate the invocation to TransportModel.
  *  
  * @author Pablo Chacin
  *
@@ -26,12 +27,36 @@ public class TransportAgent extends DynamicProxyTransport {
 	}
 
 
+	/**
+	 * Handles the invocation of a protocols's 
+	 * @param protocol
+	 * @param method
+	 * @param args
+	 */
+	void handleInvocation(String protocolName,String methodName,Object[] args) throws Exception{
+		
+		Protocol protocol = getProtocol(protocolName);
+		if(protocol == null){
+			throw new Exception("Protocol not registered: ["+protocol +"]");
+		}
+		
+		ReflectionUtils.invoke(protocol,methodName,args);
+		
+	}
+	
 	@Override
+	/**
+	 * This method is invoked by the protocol proxy to actually invoke a method in a remote
+	 * node. Delegates this invocation to the TransportModel.
+	 */
 	protected void invoke(Node target, Protocol protocol,Method method, Object[] args) throws Throwable {
 		
-		model.invoke(node,target,protocol.getName(), method, args);
+		model.invoke(this,target,protocol.getName(), method.getName(), args);
 	}
 
-
+	
+	public UnderlayNode getNode(){
+		return node;
+	}
 	
 }
--- transport/TransportModel.java
+++ transport/TransportModel.java
@@ -1,13 +1,13 @@
 package edu.upc.cnds.collectivesim.transport;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import edu.upc.cnds.collectives.node.Node;
 import edu.upc.cnds.collectives.underlay.UnderlayAddress;
 import edu.upc.cnds.collectives.underlay.UnderlayNode;
-import edu.upc.cnds.collectivesim.model.Model;
+import edu.upc.cnds.collectivesim.model.imp.AbstractModel;
 import edu.upc.cnds.collectivesim.scheduler.Scheduler;
 
 /**
@@ -20,8 +20,51 @@ import edu.upc.cnds.collectivesim.scheduler.Scheduler;
  * @author 
  *
  */
-public class TransportModel extends Model {
+public abstract class TransportModel extends AbstractModel {
 
+	private static Logger log = Logger.getLogger("collectivesim.transport");
+	
+	/**
+	 *  Implements the actual invocation of a method of a protocol in a target 
+	 *  TransportAgent
+	 */
+	private class TransportAction implements Runnable {
+
+		private TransportAgent agent;
+		
+		private String protocol;
+		
+		private String method;
+		
+		private Object[] args;
+		
+		
+		
+		TransportAction(TransportAgent agent, String protocol, String method,
+				Object[] args) {
+			super();
+			this.agent = agent;
+			this.protocol = protocol;
+			this.method = method;
+			this.args = args;
+		}
+
+
+
+		@Override
+		public void run() {
+	
+				try {
+					agent.handleInvocation(protocol, method, args);
+				} catch (Exception e) {
+					log.severe("exception invoking method in protocol");
+				}
+		}
+		
+	}
+	
+	
+	
 	/**
 	 * Mantains the mapping of transport agents to nodes
 	 */
@@ -54,20 +97,38 @@ public class TransportModel extends Model {
 
 
 	/**
-	 * Simulates the invocation on a remote node
+	 * Simulates the invocation on a remote node by scheduling its execution after
+	 * a delay. 
 	 * 
-	 * @param node location of the transport agent that makes the invocation
+	 * @param agent the TransportAgent that makes the invocation
 	 * @param target location of the transport that must handle the invocation
 	 * @param protocol the name of the protocol
 	 * @param method method to invoke
-	 * @param args arguments for the invocaton
+	 * @param args arguments for the invocation
 	 */
-	public void invoke(UnderlayNode node, Node target, String protocol,Method method,
-			Object[] args) {
-		// TODO Auto-generated method stub
-		
+	public void invoke(TransportAgent agent, Node target, String protocol,String method,
+			Object[] args){
+	
+	 TransportAgent targetAgent = transportAgents.get(target.getAddress());
+	 
+	 TransportAction action = new TransportAction(targetAgent,protocol,method,args);
+	 
+	 Long delay = getDelay(agent.getNode(),targetAgent.getNode());
+	 
+	 scheduler.scheduleAction(action,delay);
+	 
 	}
 	
-	
-	
+	/**
+	 * Calculates the delay when invoking a protocol in another node.
+	 * 
+	 * TODO: use UnderlayModel to actually calculate the delay
+	 * 
+	 * @param source 
+	 * @param target  
+	 * @return
+	 */
+	protected long getDelay(UnderlayNode source,UnderlayNode target){
+		return 1;
+	}
 }
