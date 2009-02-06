ba33f485a8e54642da551857e0b3887cb03301b7
Pablo Chacin <pablochacin@gmail.com>
2009-02-06 17:57:04 +0000

    complete base implementation

--- topology/TopologyModel.java
+++ topology/TopologyModel.java
@@ -2,51 +2,63 @@ package edu.upc.cnds.collectivesim.topology;
 
 import java.util.List;
 
-import edu.upc.cnds.collectives.node.Node;
 import edu.upc.cnds.collectives.topology.Topology;
+import edu.upc.cnds.collectives.underlay.UnderlayNode;
+import edu.upc.cnds.collectivesim.model.imp.AbstractModel;
+import edu.upc.cnds.collectivesim.scheduler.Scheduler;
 
 /**
  * 
- * Builds and mantains a topology of nodes, generating the local topology view for
- * each node. 
+ * Builds and maintains a topology of nodes, generating the local topology view for
+ * each node. This view is maintained by a TopologyModelAgent.
  * 
  * @author Pablo Chacin
  *
  */
-public interface TopologyModel {
+public abstract class TopologyModel extends AbstractModel {
 	
-	/**
-	 * Adds the node to the topology
-	 * 
-	 * @param node a Node that will form part of the topology
-	 * 
-	 * @throws TopologyModelException if the node cannot be added to
-	 *         the topology (e.g. lack of space)
-	 */
-	public void addNode(Node node) throws TopologyModelException;
+	protected List<UnderlayNode>nodes; 
 		
+	public TopologyModel(Scheduler scheduler,List<UnderlayNode>nodes) {
+		super(scheduler);
+		this.nodes = nodes;
+		addBehavior("Topology Update", "updateTopology", true,100);
+	}
+
 	/**
-	 * Generates the local view of the topology for a particular node
-	 * @param node Node to
+	 * Generates the local view of the topology for a particular UnderlayNode
+	 * 
+	 * @param node the UnderlayNode to generate the view for.
+	 * 
 	 * @return the local view of the topology for a node
 	 * 
 	 */
-	public Topology getTopologyModelProxy(Node node);
+	public Topology getTopology(UnderlayNode node){
+		TopologyAgent agent = getTopologyAgent(node);
+		super.addAgent(agent);
+		return agent;
+	}
+	
 	
 	/**
-	 * 
-	 * @return a list of the nodes currently in the topology
+	 * Generates a topology
 	 */
-	public List<Node> getNodes();
+	public abstract void generateTopology();
 	
 	/**
-	 * Removes a node from the topology. All other nodes' local views
-	 * are updated
+	 * Adds node to the topology
 	 * 
-	 * @param node to remove
-	 * 
-	 * @throws TopologyModelException if the node couldn't be removed (for instance
-	 *         if it currently doesn't belongs to the topology)
+	 * @param node
 	 */
-	public void removeNode(Node node) throws TopologyModelException;
+	public void addNode(UnderlayNode node){
+		nodes.add(node);
+	}
+	
+	/**
+	 * Construct a topology agent for the given node
+	 * 
+	 * @param node
+	 * @return
+	 */
+	protected abstract TopologyAgent getTopologyAgent(UnderlayNode node) ;
 }
