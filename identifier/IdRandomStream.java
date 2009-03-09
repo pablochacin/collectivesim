e5b837596e772fff637221334073c0aa90a57f8d
Pablo Chacin <pablochacin@gmail.com>
2009-03-09 23:36:36 +0000

    implement helper classes to generate id streams

---
+++ identifier/IdRandomStream.java
@@ -0,0 +1,37 @@
+package edu.upc.cnds.collectivesim.identifier;
+
+import edu.upc.cnds.collectives.identifier.IdSpace;
+import edu.upc.cnds.collectives.identifier.Identifier;
+import edu.upc.cnds.collectivesim.model.Stream;
+
+/**
+ * Generates a random stream of Identifiers from an IdSpace.
+ * 
+ * @author Pablo Chacin
+ *
+ */
+public class IdRandomStream implements Stream<Identifier> {
+
+	private String name;
+	
+	private IdSpace space;
+	
+
+	
+	public IdRandomStream(String name, IdSpace space) {
+		super();
+		this.name = name;
+		this.space = space;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Identifier getValue() {
+		return space.getRandomIdentifier();
+	}
+
+}
