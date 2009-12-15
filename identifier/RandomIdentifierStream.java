72d706b60e4c06ae33498e2b116c00b6145cd0b0
Pablo Chacin <pablochacin@gmail.com>
2009-12-15 18:10:39 +0000

    generate identifiers from a random stream of byte arrays provided by the MersseneRandom geneator

---
+++ identifier/RandomIdentifierStream.java
@@ -0,0 +1,66 @@
+package edu.upc.cnds.collectivesim.identifier;
+
+import java.util.Random;
+
+import cern.jet.random.engine.MersenneTwister64;
+import edu.upc.cnds.collectives.identifier.BasicIdentifier;
+import edu.upc.cnds.collectives.identifier.IdSpace;
+import edu.upc.cnds.collectives.identifier.Identifier;
+import edu.upc.cnds.collectivesim.random.MersenneRandom;
+import edu.upc.cnds.collectivesim.stream.Stream;
+
+/**
+ * Generates a random stream of Identifiers from an IdSpace.
+ * 
+ * @author Pablo Chacin
+ *
+ */
+public class RandomIdentifierStream implements Stream<Identifier> {
+
+	private String name;
+	
+
+	private int size;
+	
+	private Random rnd = new MersenneRandom();
+
+	
+	public RandomIdentifierStream(String name,int size) {
+		super();
+		this.name = name;
+		this.size = size;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+
+
+	@Override
+	public void reset() {
+		//Do nothing. Reseting this stream has no effect
+	}
+
+	@Override
+	public void open() {
+		// Do nothing.
+		
+	}
+
+	@Override
+	public boolean hasMoreElements() {
+		return true;
+	}
+
+	@Override
+	public Identifier nextElement() {
+		byte[] bytes = new byte[size];
+		
+		rnd.nextBytes(bytes);
+		
+		return new BasicIdentifier(bytes);
+	}
+
+}
