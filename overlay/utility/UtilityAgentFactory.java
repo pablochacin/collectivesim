9ba0740a0f32985efa12e26b8524400f4bf206a7
Pablo Chacin <pablochacin@gmail.com>
2010-04-13 17:50:10 +0000

    set random utility to a random walk

--- overlay/utility/UtilityAgentFactory.java
+++ overlay/utility/UtilityAgentFactory.java
@@ -62,13 +62,13 @@ public class UtilityAgentFactory extends OverlayAgentFactory {
 
 	protected UtilityFunction getUtilityFunction() {
 		
-//		UtilityFunction function = new RandomWalkUtilityFunction(utility.nextElement(), 
-//                initialTrend.nextElement(),
-//                variation, drift.nextElement(), 
-//                trend);
+		UtilityFunction function = new RandomWalkUtilityFunction(utility.nextElement(), 
+                initialTrend.nextElement(),
+                variation, drift.nextElement(), 
+                trend);
 		
-		UtilityFunction function = new WhiteNoiseUtilityFunction(utility.nextElement(), 
-										variation);
+//		UtilityFunction function = new WhiteNoiseUtilityFunction(utility.nextElement(), 
+//										variation);
 
 		return function;
 	}
