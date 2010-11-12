131b4353991cdbc7960e6117e669466a469a7356
Pablo Chacin <pablochacin@gmail.com>
2010-11-12 20:17:21 +0000

    refactor events to remove association with Nodes, adding a generic "location"

--- overlay/webservices/ServiceExecutionEvent.java
+++ overlay/webservices/ServiceExecutionEvent.java
@@ -3,23 +3,15 @@ package edu.upc.cnds.collectivesim.overlay.webservices;
 import java.util.Map;
 
 import edu.upc.cnds.collectives.events.imp.BasicEvent;
-import edu.upc.cnds.collectives.node.Node;
+import edu.upc.cnds.collectivesim.overlay.service.ServiceOverlayAgent;
 
 
 public class ServiceExecutionEvent extends BasicEvent {	
 	
 	public static String SERVICE_EVENT_SERVICED= "collectives.service.processed";
 	
-	public ServiceExecutionEvent(Node node, long timeStamp, Map attributes, Object... data) {
-		super(node, SERVICE_EVENT_SERVICED, timeStamp, attributes, data);
-	}
-
-	public ServiceExecutionEvent(Node node, long timeStamp, Object... data) {
-		super(node, SERVICE_EVENT_SERVICED, timeStamp, data);
-	}
-
-	public ServiceExecutionEvent(Node node, long timeStamp) {
-		super(node, SERVICE_EVENT_SERVICED, timeStamp);
+	public ServiceExecutionEvent(ServiceOverlayAgent agent, long timeStamp, Map attributes ) {
+		super(agent.getName(), timeStamp,SERVICE_EVENT_SERVICED,  attributes);
 	}
 
 }
--- overlay/webservices/ServiceReceptionEvent.java
+++ overlay/webservices/ServiceReceptionEvent.java
@@ -3,23 +3,20 @@ package edu.upc.cnds.collectivesim.overlay.webservices;
 import java.util.Map;
 
 import edu.upc.cnds.collectives.events.imp.BasicEvent;
-import edu.upc.cnds.collectives.node.Node;
+import edu.upc.cnds.collectivesim.overlay.service.ServiceOverlayAgent;
 
 
 public class ServiceReceptionEvent extends BasicEvent {	
 	
 	public static String SERVICE_EVENT_RECIEVED= "collectives.service.recieved";
 	
-	public ServiceReceptionEvent(Node node, long timeStamp, Map attributes, Object... data) {
-		super(node, SERVICE_EVENT_RECIEVED, timeStamp, attributes, data);
+	public ServiceReceptionEvent(ServiceOverlayAgent agent, long timeStamp, Map attributes) {
+		super(agent.getName(), timeStamp,SERVICE_EVENT_RECIEVED,  attributes);
 	}
 
-	public ServiceReceptionEvent(Node node, long timeStamp, Object... data) {
-		super(node, SERVICE_EVENT_RECIEVED, timeStamp, data);
-	}
 
-	public ServiceReceptionEvent(Node node, long timeStamp) {
-		super(node, SERVICE_EVENT_RECIEVED, timeStamp);
+	public ServiceReceptionEvent(ServiceOverlayAgent agent, long timeStamp) {
+		super(agent.getName(), timeStamp, SERVICE_EVENT_RECIEVED);
 	}
 
 }
